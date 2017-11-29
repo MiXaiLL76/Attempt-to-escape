f74b87380c685be4be2b5a16e85be56541012593
mixaill76 <mike.milos@yandex.ru>
2017-11-29 16:51:01 +0300

    okey, lets go home

--- main.java
+++ main.java
@@ -32,47 +32,54 @@ class Attempt_to_escape {
         System.out.println("Prisoner: "+prisoner[0]+"x"+prisoner[1]);
         System.out.println("Exit: "+exit[0]+"x"+exit[1]);
 
-        char[][] new_castle = new char[m][n];
+        String[][] new_castle = new String[m][n];
         
         for(int i=0;i<m;i++){
             for(int w=0;w<n;w++){
-                new_castle[i][w] = castle.get(i).replace('0','#').replace('1','?').charAt(w);
+                new_castle[i][w] = String.valueOf(castle.get(i).replace("0","#").replace("1","?").charAt(w));
             }            
         }
-        new_castle[m-1][0] = '0';//Prisoner add  
-        new_castle[0][n-1] = 'E';//Exit add
-        new_castle[0][0] = '2';
+        new_castle[m-1][0] = "0";//Prisoner add  
+        new_castle[0][n-1] = "E";//Exit add
         int next = 0;
         while(next < n*m)
         {            
+            int chard = 0;
             for(int i=0;i<m;i++){
                 for(int w=0;w<n;w++){
-                    if(new_castle[i][w] == Character.forDigit(next, 10)){
-                        if(i+1<m && new_castle[i+1][w]=='?'){
-                            new_castle[i+1][w] = String.valueOf(next+1).charAt(0);
+                    if(new_castle[i][w].charAt(0) == Character.forDigit(next,10)){
+                        if(i+1<m && new_castle[i+1][w].charAt(0) == '?'){
+                            new_castle[i+1][w] = String.valueOf(next+1);
                         }
-                        if(i-1>=0 && new_castle[i-1][w]=='?'){
-                            new_castle[i-1][w] = String.valueOf(next+1).charAt(0);
+                        if(i-1>=0 && new_castle[i-1][w].charAt(0) == '?'){
+                            new_castle[i-1][w] = String.valueOf(next+1);
                         }
-                        if(w+1<n && new_castle[i][w+1]=='?'){
-                            new_castle[i][w+1] = String.valueOf(next+1).charAt(0);
+                        if(w+1<n && new_castle[i][w+1].charAt(0) == '?'){
+                            new_castle[i][w+1] = String.valueOf(next+1);
                         }
-                        if(w-1>=0 && new_castle[i][w-1]=='?'){
-                            new_castle[i][w-1] = String.valueOf(next+1).charAt(0);
+                        if(w-1>=0 && new_castle[i][w-1].charAt(0) == '?'){
+                            new_castle[i][w-1] = String.valueOf(next+1);
                         }
                     }
+                    
+                    if(new_castle[i][w].charAt(0) == '?')chard+=1;
+                    
                 }       
             }
+            if(chard == 0)break;
             next++;
         }
-
+        
+        System.out.println();
+        System.out.println(Character.forDigit(next,10));
+        System.out.println();
         for(int i=0;i<m;i++){
             for(int w=0;w<n;w++){
-                System.out.print(new_castle[i][w]);
+                System.out.print(new_castle[i][w]+" ");
             }    
             System.out.println();       
         }
-       
+        
         
 
         /*
@@ -84,6 +91,12 @@ class Attempt_to_escape {
             2  3 4  5 -2
             1 -1 5 -1 9
             0 -1 6  7 8
+            4 5 6 7 8 9 10 E 
+            3 4 5 6 7 8  # ? 
+            2 3 # # # 9 10 # 
+            1 # 3 4 5 #  9 # 
+            0 1 2 # 6 7  8 # 
+
         */
     }
 }
